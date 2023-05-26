37d733207c87c03773e5dfc7a4bdcb83dbc3f60d
Badylek123 <badylekzosia@gmail.com>
2023-05-26 16:38:29 +0200

    Add flagging option to game

--- minesweppercli.cpp
+++ minesweppercli.cpp
@@ -12,13 +12,16 @@
 using namespace std;
 
 // Cell structure to represent each cell on the board
-struct Cell {
+struct Cell 
+{
 	bool isMine;
 	bool isRevealed;
 	int adjacentMines;
+	bool isFlag;
 };
 
-class Minesweeper {
+class Minesweeper 
+{
 private:
 	int rows;
 	int columns;
@@ -28,38 +31,42 @@ private:
 public:
 	Minesweeper(int rows, int columns, int mines) 
 	{
-        	this->rows = rows;
-        	this->columns = columns;
-        	totalMines = mines;
-        	board.resize(rows, vector<Cell>(columns, {false, false, 0}));
+        this->rows = rows;
+    	this->columns = columns;
+    	totalMines = mines;
+        board.resize(rows, vector<Cell>(columns, {false, false, 0, false}));
 	}
-	void placeMines() {
-
+	void placeMines() 
+    {
         int count = 0;
-        while (count < totalMines) {
+        while (count < totalMines) 
+        {
             int row = rand() % rows;
             int col = rand() % columns;
-            if (!board[row][col].isMine) {
+            if (!board[row][col].isMine) 
+            {
                 board[row][col].isMine = true;
                 count++;
             }
         }
     }
 
-    void calculateAdjacentMines() {
+    void calculateAdjacentMines() 
+    {
         int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
         int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
 
         for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                if (!board[i][j].isMine) {
+            for (int j = 0; j < columns; j++) 
+            {
+                if (!board[i][j].isMine) 
+                {
                     int count = 0;
-                    for (int k = 0; k < 8; k++) {
+                    for (int k = 0; k < 8; k++) 
+                    {
                         int ni = i + dx[k];
                         int nj = j + dy[k];
-                        if (isValid(ni, nj) && board[ni][nj].isMine) {
-                            count++;
-                        }
+                        if (isValid(ni, nj) && board[ni][nj].isMine) count++;
                     }
                     board[i][j].adjacentMines = count;
                 }
@@ -67,22 +74,22 @@ public:
         }
     }
 
-    bool isValid(int row, int col) {
+    bool isValid(int row, int col)
+    {
         return (row >= 0 && row < rows && col >= 0 && col < columns);
     }
-
-    void revealCell(int row, int col) {
-        if (!isValid(row, col) || board[row][col].isRevealed) {
-            return;
-        }
+    void revealCell(int row, int col) // gonna change this cause i repeated myself, but clean the mess first 
+    {
+        if (!isValid(row, col) || board[row][col].isRevealed || board[row][col].isFlag) return;
 
         board[row][col].isRevealed = true;
 
-        if (board[row][col].adjacentMines == 0) {
+        if (board[row][col].adjacentMines == 0) 
+        {
             int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
             int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
-
-            for (int k = 0; k < 8; k++) {
+            for (int k = 0; k < 8; k++) 
+            {
                 int ni = row + dx[k];
                 int nj = col + dy[k];
                 revealCell(ni, nj);
@@ -90,90 +97,114 @@ public:
         }
     }
 
-    void gameOver() {
-	clearScreen;
-        cout << "Trafiles na mine.\nJuz nawet nie ma co zbierac.\nKoniec gry." << endl;
+    void gameOver() 
+    {
+	    clearScreen;
+        cout << "Trafiles na mine.\nJuz nawet nie ma co zbierac.\nKoniec gry." << '\n';
         // Display the full board
         revealAllCells();
         displayBoard();
     }
 
-    void revealAllCells() {
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
+    void revealAllCells() 
+    {
+        for (int i = 0; i < rows; i++) 
+        {
+            for (int j = 0; j < columns; j++) 
+            {
                 board[i][j].isRevealed = true;
             }
         }
     }
 
-    bool isGameWon() {
+    bool isGameWon() 
+    {
         int unrevealedCount = 0;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                if (!board[i][j].isRevealed && !board[i][j].isMine) {
+        for (int i = 0; i < rows; i++) 
+        {
+            for (int j = 0; j < columns; j++) 
+            {
+                if (!board[i][j].isRevealed && !board[i][j].isMine)
                     unrevealedCount++;
-                }
             }
         }
         return unrevealedCount == 0;
     }
 
-    void displayBoard() {
+    void displayBoard() 
+    {
         cout << "  ";
-        for (int j = 0; j < columns; j++) {
+        for (int j = 0; j < columns; j++) 
+        {
             cout << j << " ";
         }
-        cout << endl;
-
-        for (int i = 0; i < rows; i++) {
+        cout << '\n';
+        for (int i = 0; i < rows; i++) 
+        {
             cout << i << " ";
-            for (int j = 0; j < columns; j++) {
-                if (!board[i][j].isRevealed) {
-                    cout << "- ";
-                } else {
-                    if (board[i][j].isMine) {
-                        cout << "* ";
-                    } else {
-                        cout << board[i][j].adjacentMines << " ";
-                    }
-                }
+            for (int j = 0; j < columns; j++) 
+            {
+                if (!board[i][j].isRevealed && !board[i][j].isFlag)	    cout << "- ";
+		        else if (board[i][j].isRevealed && board[i][j].isMine)	cout << "* ";
+		        else if (board[i][j].isFlag)				            cout << "! ";
+		        else							                        cout << board[i][j].adjacentMines << " ";
             }
-            cout << endl;
+            cout << '\n';
         }
+        
     }
 
-    void play() {
+
+    void play() 
+    {
         placeMines();
         calculateAdjacentMines();
 
-        while (true) {
-	clearScreen;    
-	displayBoard();
-
+        while (true) 
+        {
+            clearScreen;    
+            displayBoard();
+            char flagOrRev;
+            string dump;
             int row, col;
+            cout << "Wybierz opcje: (O)dsłoń, (F)laguj\n";
+            cin >> flagOrRev;
+            if (flagOrRev!='O' && flagOrRev!='o' && flagOrRev!='F' && flagOrRev!='f') flagOrRev=0;
             cout << "Podaj wspolrzedne pola (rzad <spacja> kolumna) : ";
             cin >> row >> col;
-
-            if (!isValid(row, col)) {
-                cout << "Cos nie wyszlo (bledne dane?). Sprobuj ponownie" << endl;
+            if (!isValid(row, col) || flagOrRev==0) 
+            {
+                cout << "Cos nie wyszlo (bledne dane?). Sprobuj ponownie" << '\n';
+                cin >> dump;
                 continue;
             }
-
-            revealCell(row, col);
-	    if (board[row][col].isMine)
-	    {
-		 gameOver();
-		 break;
-	    }
-            if (isGameWon()) {
-                cout << "Przezyles! Gratulacje przesyla Badyl." << endl;
+            if ((flagOrRev == 'o' || flagOrRev == 'O') && !board[row][col].isFlag)
+                revealCell(row, col);
+            else
+                board[row][col].isFlag = !board[row][col].isFlag;
+            if (board[row][col].isMine && board[row][col].isRevealed) 
+            {
+                gameOver();
+                break;
+            }
+            //if((flagOrRev=='o' || flagOrRev=='O') || !board[row][col].isFlag) 	revealCell(row, col);
+            //else if(flagOrRev=='f' || flagOrRev=='F')                           board[row][col].isFlag=true;
+            //if (board[row][col].isMine)
+            //{
+            //    gameOver();
+            //    break;
+            //}
+            if (isGameWon()) 
+            {
+                cout << "Przezyles! Gratulacje przesyla Badyl." << '\n';
                 break;
             }
         }
     }
 };
 
-int main() {
+int main() 
+{
     srand(static_cast<unsigned>(time(0)));
 
     int rows, columns, mines;
